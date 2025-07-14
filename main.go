c16edf05fc3359db243cb6633c3c3ce813643df1
ZeroNine <zero@nine.com>
2025-07-15 03:15:46 +0500

    finished section A

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
 
-func main(){
+	"super-octo-engine.com/octo/handlers"
+	"super-octo-engine.com/octo/logger"
+)
 
-	fmt.Println("Hello world")
+func main() {
+	// Initialize logger
+	logInstance := initializeLogger()
+	movieHandler := handlers.MovieHandler{}
 
+	http.HandleFunc("/api/movies/top", movieHandler.GetTopMovies)
+
+	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	// Start server
+	const addr = ":8080"
+	logInstance.Info("Server starting on " + addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		logInstance.Error("Server failed to start", err)
+		log.Fatalf("Server failed: %v", err)
+	}
+}
+
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movie-service.log")
+	if err != nil {
+		log.Fatalf("Failed to initialize logger: %v", err)
+	}
+	return logInstance
 }
